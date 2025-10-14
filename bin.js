8e5779e02257b6f9f27877ab3709ac4e0b5accb1
hdegroote <75906619+HDegroote@users.noreply.github.com>
2025-10-14 09:28:33 +0200

    Make module

--- bin.js
+++ bin.js
@@ -1,108 +1,53 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
 const path = require('path')
-const WS = require('ws')
 const b4a = require('b4a')
+const InspectorRepl = require('.')
 
-const socket = new WS(process.argv[2])
-
-let heapdumpLocation = null
-let heapdumpMessageId = -1
-let cpuProfileLocation = null
-let cpuProfileMessageId = null
-
-socket.on('message', function (data) {
-  const m = JSON.parse(data)
-  if (m.method === 'Console.messageAdded') {
-    console.log(m.params.message.text)
-  } else if (m.method === 'HeapProfiler.addHeapSnapshotChunk') {
-    fs.appendFileSync(heapdumpLocation, m.params.chunk)
-  } else if (!m.method && m.result) {
-    if (m.id === heapdumpMessageId) {
-      console.log(`Heapdump finished at ${heapdumpLocation}`)
-    } else if (m.id === cpuProfileMessageId) {
-      console.log(`Saving CPU profile to ${cpuProfileLocation}`)
-      fs.promises.writeFile(cpuProfileLocation, JSON.stringify(m.result.profile))
-        .then(() => { console.log('Saved CPU profile')})
-        .catch((e) => console.error(e))
-    } else {
-      console.log(m.result.result?.value)
-    }
-  }
+const repl = new InspectorRepl(process.argv[2])
+repl.on('console-message', m => {
+  console.log(m)
+})
+repl.on('heapdump-done', loc => {
+  console.log(`Heapdump finished at ${loc}`)
+})
+repl.on('cpu-profile-saved', loc => {
+  console.log(`CPU profile finished at ${loc}`)
+})
+repl.on('cpu-profile-error', err => {
+  console.warn(`CPU profile errored: ${err.stack}`)
 })
 
-socket.on('open', function () {
-  socket.send(JSON.stringify({
-    id: 0,
-    method: 'Console.enable'
-  }))
-
-  socket.send(JSON.stringify({
-    id: 1,
-    method: 'HeapProfiler.enable'
-  }))
-
-  socket.send(JSON.stringify({
-    id: 2,
-    method: 'Profiler.enable'
-  }))
-
-
-  let id = 3
-
-  process.stdin.on('data', function (data) {
-    const cleanData = b4a.toString(data).trim()
-    if (cleanData.startsWith('heapdump')) {
-      heapdumpLocation = cleanData.split(' ')[1]
-      if (!heapdumpLocation) {
-        const timestamp = new Date(Date.now()).toISOString().split('.')[0].replaceAll(':', '-')
-        heapdumpLocation = `inspector-repl-${timestamp}.heapsnapshot`
-      }
-      heapdumpLocation = path.resolve(heapdumpLocation)
-      console.log(`Creating heapdump at ${heapdumpLocation}`)
-
-      heapdumpMessageId = id
-      socket.send(JSON.stringify({
-        id: id++,
-        method: 'HeapProfiler.takeHeapSnapshot',
-      }))
-
-      return
+process.stdin.on('data', function (data) {
+  const cleanData = b4a.toString(data).trim()
+  if (cleanData.startsWith('heapdump')) {
+    let heapdumpLocation = cleanData.split(' ')[1]
+    if (!heapdumpLocation) {
+      const timestamp = new Date(Date.now()).toISOString().split('.')[0].replaceAll(':', '-')
+      heapdumpLocation = `inspector-repl-${timestamp}.heapsnapshot`
     }
+    heapdumpLocation = path.resolve(heapdumpLocation)
+    console.log(`Creating heapdump at ${heapdumpLocation}`)
 
-    if (cleanData.startsWith('profiler')) {
-      const profileLength = cleanData.split(' ')[1] || 5_000
-      cpuProfileLocation = cleanData.split(' ')[2]
-      if (!cpuProfileLocation) {
-        const timestamp = new Date(Date.now()).toISOString().split('.')[0].replaceAll(':', '-')
-        cpuProfileLocation = `inspector-repl-${timestamp}.cpuprofile`
-      }
-      cpuProfileLocation = path.resolve(cpuProfileLocation)
-      console.log(`Creating CPU profile of ${profileLength}ms at ${cpuProfileLocation}`)
+    repl.heapdump(heapdumpLocation)
+    return
+  }
 
-      socket.send(JSON.stringify({
-        id: id++,
-        method: 'Profiler.start',
-      }))
-
-      setTimeout(() => {
-        cpuProfileMessageId = id++
-        socket.send(JSON.stringify({
-          id: cpuProfileMessageId,
-          method: 'Profiler.stop'
-        }))
-      }, profileLength)
-
-      return
+  if (cleanData.startsWith('profiler')) {
+    const profileLength = cleanData.split(' ')[1] || 5_000
+    let cpuProfileLocation = cleanData.split(' ')[2]
+    if (!cpuProfileLocation) {
+      const timestamp = new Date(Date.now()).toISOString().split('.')[0].replaceAll(':', '-')
+      cpuProfileLocation = `inspector-repl-${timestamp}.cpuprofile`
     }
+    cpuProfileLocation = path.resolve(cpuProfileLocation)
+    console.log(`Creating CPU profile of ${profileLength}ms at ${cpuProfileLocation}`)
 
-    socket.send(JSON.stringify({
-      id: id++,
-      method: 'Runtime.evaluate',
-      params: { expression: '' + data.toString().trim() }
-    }))
-  })
+    repl.profile(profileLength, cpuProfileLocation)
+    return
+  }
+
+  repl.evaluate('' + data.toString().trim())
 })
 
 console.log('Available commands:\n- heapdump <location>?\n- profiler <lengthMs>? <location>?')
---
+++ index.js
@@ -0,0 +1,90 @@
+const { EventEmitter } = require('events')
+const fs = require('fs')
+const WS = require('ws')
+
+class InspectorRepl extends EventEmitter {
+  constructor (wsUrl) {
+    super()
+
+    this.socket = new WS(wsUrl)
+    this._heapdumpMessageId = null
+    this._cpuProfileMessageId = null
+    this._msgId = 0
+    this._heapdumpLocation = null
+    this._cpuProfileLocation = null
+
+    this.socket.on('message', this._onmessage.bind(this))
+    this.socket.on('open', () => {
+      this.socket.send(JSON.stringify({
+        id: 0,
+        method: 'Console.enable'
+      }))
+
+      this.socket.send(JSON.stringify({
+        id: 1,
+        method: 'HeapProfiler.enable'
+      }))
+
+      this.socket.send(JSON.stringify({
+        id: 2,
+        method: 'Profiler.enable'
+      }))
+
+      this._msgId += 3
+    })
+  }
+
+  _onmessage (data) {
+    const m = JSON.parse(data)
+    if (m.method === 'Console.messageAdded') {
+      this.emit('console-message', m.params.message.text)
+    } else if (m.method === 'HeapProfiler.addHeapSnapshotChunk') {
+      fs.appendFileSync(this._heapdumpLocation, m.params.chunk)
+    } else if (!m.method && m.result) {
+      if (m.id === this._heapdumpMessageId) {
+        this.emit('heapdump-done', this._heapdumpLocation)
+      } else if (m.id === this._cpuProfileMessageId) {
+        console.log('writing to', this._cpuProfileLocation, m)
+        fs.promises.writeFile(this._cpuProfileLocation, JSON.stringify(m.result.profile))
+          .then(() => { this.emit('cpu-profile-saved', this._cpuProfileLocation) })
+          .catch((e) => { this.emit('cpu-profile-error', e) })
+      } else {
+        this.emit('console-message', m.result.result?.value)
+      }
+    }
+  }
+
+  heapdump (location) {
+    this._heapdumpLocation = location
+    this._heapdumpMessageId = this._msgId
+    this.socket.send(JSON.stringify({
+      id: this._msgId++,
+      method: 'HeapProfiler.takeHeapSnapshot'
+    }))
+  }
+
+  profile (length, location) {
+    this._cpuProfileLocation = location
+    this._cpuProfileMessageId = this._msgId + 1
+    this.socket.send(JSON.stringify({
+      id: this._msgId+=2, // also for the response
+      method: 'Profiler.start'
+    }))
+    setTimeout(() => {
+      this.socket.send(JSON.stringify({
+        id: this._cpuProfileMessageId,
+        method: 'Profiler.stop'
+      }))
+    }, length)
+  }
+
+  evaluate (expression) {
+    this.socket.send(JSON.stringify({
+      id: this._msgId++,
+      method: 'Runtime.evaluate',
+      params: { expression }
+    }))
+  }
+}
+
+module.exports = InspectorRepl
